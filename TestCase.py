71ad49049c45b3fcc30a117c468b114e16dc98bc
Ivan <iv.cherepanov@yandex.ru>
2022-09-18 14:03:27 +0300

    seond test&function

--- TestCase.py
+++ TestCase.py
@@ -8,3 +8,8 @@ class ParsingTestCase(unittest.TestCase):
         get_HTML_page()
         assert os.path.exists("data_base_cities.html") != False
         assert os.path.getsize("data_base_cities.html") > 0
+
+    def test_parse_to_file(self):
+        parse_to_file()
+        assert os.path.exists("data_base_cities.html") != False
+        assert os.path.getsize("data_base_cities.html") > 0
--- parsingCities.py
+++ parsingCities.py
@@ -7,3 +7,15 @@ def get_HTML_page():
     r = requests.get(URL)
     with open("data_base_cities.html", "w", encoding="utf-8") as f:
         f.write(r.text)
+
+def parse_to_file():
+    with open("data_base_cities.html", "r", encoding="utf-8") as f:
+        soup = BeautifulSoup(f,"html.parser")
+
+    #sort column
+    table = soup.find("table", attrs={"class": "standard sortable"})
+
+    with open("cities.txt", "w", encoding="utf-8") as f:
+        for row in table.find_all("tr")[2:]:
+            city = row.find_all("td")[2].get_text().strip()
+            f.write(f"{city}\n")
